0838e958d3e9b0dc80ceb13a5bb94e684280fc17
cighao <cighao@gmail.com>
2019-09-30 16:15:58 +0300

    update

--- src/java/org/apache/cassandra/db/commitlog/AbstractCommitLogService.java
+++ src/java/org/apache/cassandra/db/commitlog/AbstractCommitLogService.java
@@ -164,9 +164,11 @@ public abstract class AbstractCommitLogService
                 if (!sync())
                     break;
             }
+            
             System.out.println("wait sync time: " + wait_sync_time.get());   // ch add
             System.out.println("sync time: " + sync_time.get());   // ch add
             System.out.println("num: " + num.get());
+
         }
 
         boolean sync()
